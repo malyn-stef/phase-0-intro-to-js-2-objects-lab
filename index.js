c8ad9aed5a523ed496e29f78feea2b87b858fb9b
+ <+>
2023-01-15 11:45:55 -0700

    completed objects lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+
+const employee = {
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = { ...obj }
+    newObj[key] = value;
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newObj = { ...obj };
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj;
+}
+
+updateEmployeeWithKeyAndValue(employee, name, "Sam");
+updateEmployeeWithKeyAndValue(employee, streetAddress, "11 Broadway");
+destructivelyUpdateEmployeeWithKeyAndValue(employee, name, "Robert");
+deleteFromEmployeeByKey(employee, "Robert");
+destructivelyDeleteFromEmployeeByKey(employee, "Robert");
